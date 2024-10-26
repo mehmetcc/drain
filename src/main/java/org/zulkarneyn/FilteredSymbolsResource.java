e9ceb7f44bffdd5b562862f637f1a4dc618f5186
mehmetcc <mehmetccm@gmail.com>
2024-10-26 22:11:10 +0300

    Add exception handling

--- src/main/java/org/zulkarneyn/FilteredSymbolsResource.java
+++ src/main/java/org/zulkarneyn/FilteredSymbolsResource.java
@@ -5,6 +5,7 @@ import io.smallrye.mutiny.Uni;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -15,14 +16,32 @@ public class FilteredSymbolsResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Uni<FilteredSymbols> getSymbols() {
-        return service.get();
+    public Uni<Response> getSymbols() {
+        return service.get()
+                .flatMap(this::success)
+                .onFailure()
+                .recoverWithUni(failure -> failure("redis: get operation failed."));
     }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Uni<Boolean> postSymbols(FilteredSymbols symbols) {
-        return service.set(symbols);
+    public Uni<Response> postSymbols(FilteredSymbols symbols) {
+        return service.set(symbols).flatMap(object -> {
+            if (!object) return failure("redis: set operation failed.");
+
+            return Uni.createFrom().item(Response.ok(Boolean.TRUE).build());
+        });
+    }
+
+    private Uni<Response> success(FilteredSymbols symbols) {
+        return Uni.createFrom()
+                .item(Response.ok(symbols).build());
+    }
+
+    private Uni<Response> failure(String message) {
+        return Uni.createFrom()
+                .item(Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(message).build())
+                .log();
     }
 }
